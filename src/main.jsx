405be3af7bb475e1e2378601e070c2a5d8a7d0bd
Travis <tjmunyer@gmail.com>
2025-05-26 18:39:38 -0500

    Improves 404 redirect

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <BrowserRouter basename="/">
-          <App />
-      </BrowserRouter>
-  </StrictMode>,
-)
+const savedPath = sessionStorage.getItem('redirectPath');
+if (savedPath) {
+    sessionStorage.removeItem('redirectPath');
+    window.history.replaceState(null, '', savedPath);
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
