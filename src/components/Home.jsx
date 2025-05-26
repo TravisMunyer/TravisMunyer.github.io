9b0b1aaf12fb02b8cfaf54ab8a07c885369abde0
Travis <tjmunyer@gmail.com>
2025-05-26 14:02:24 -0500

    Add improved home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactGA from 'react-ga4';
+import { ReactTyped } from "react-typed";
 
 ReactGA.initialize('G-Q4JHKLZ907');
+
+const roles = [
+    "Engineer",
+    "Data Scientist",
+    "Researcher",
+    "Mentor",
+    "AI Enthusiast"
+];
 
 export default function Home() {
     ReactGA.send({ hitType: "pageview", page: "/home" });
 
     return (
-        <h1>Welcome to my website!</h1>
+        <div style={{ textAlign: 'center' }}>
+            <h1 style={{ fontSize: '2.5em', marginBottom: '0.5em' }}>Meet Travis.</h1>
+            <ReactTyped
+                strings={roles}
+                typeSpeed={60}
+                backSpeed={40}
+                backDelay={1500}
+                loop
+                showCursor
+                cursorChar="|"
+                style={{ fontSize: '2em', color: '#555' }}
+            />
+        </div>
     );
 }
